ff157a45054ac5b63512f5b79c38a4ca62942806
breakfastitem <andrew.ehrman@gmail.com>
2021-03-09 15:25:28 -0500

    Added basic filter form to dom

---
+++ src/components/FilterForm/FilterForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function FilterForm() {
+
+    return (
+        <form>
+        <input placeholder="filter" id="filter">
+        </input>
+        <button>filter</button>
+        </form>
+    );
+}
+
+export default FilterForm;
